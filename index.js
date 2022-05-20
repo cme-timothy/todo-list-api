20a4620203afdc72a35b8d7183cd00f03ce36be6
Timothy Magee <oaklessons@gmail.com>
2022-05-20 16:34:12 +0200

    added delete todo endpoint

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const server = http.createServer((req, res) => {
   console.log(items.length);
   console.log(`${req.method} to url: ${req.url}`);
 
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, DELETE, OPTIONS, POST, PUT, PATCH"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
   if (req.url === "/api/todos") {
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    if (req.method === "GET" && items.length === 3) {
+    if (req.method === "GET") {
       res.statusCode = 200;
       res.end(JSON.stringify(todos));
-    } else if (req.method === "GET") {
-      const requestedId = parseInt(items[3]);
-      const requestedTodo = todos.find((todos) => todos.id === requestedId);
-
-      res.statusCode = 200;
-      res.end(JSON.stringify(requestedTodo));
     }
     if (req.method === "POST") {
       req.on("data", (chunk) => {
@@ -34,6 +34,21 @@ const server = http.createServer((req, res) => {
         todos.push(newTodo);
       });
       res.statusCode = 201;
+      res.end();
+    }
+  }
+  if (items.length === 4) {
+    if (req.method === "GET") {
+      const requestedId = parseInt(items[3]);
+      const requestedTodo = todos.find((todos) => todos.id === requestedId);
+
+      res.statusCode = 200;
+      res.end(JSON.stringify(requestedTodo));
+    }
+    if (req.method === "DELETE") {
+      todos = todos.filter((todo) => todo.id !== items[3]);
+
+      res.statusCode = 204;
       res.end();
     }
   }
